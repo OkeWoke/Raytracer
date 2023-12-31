a0d7a0bb8cf939ccfc053bf11f75076ab9bd5915
OkeWoke <l.murphy9002@gmail.com>
2023-12-31 15:08:17 +1300

    fix: fix missing non tracked files

---
+++ modules/Utility/tests/VectorTests.cpp
@@ -0,0 +1,17 @@
+#include "Vector.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Vector Operations", "[Vector]")
+{
+    Vector a(1,2,3);
+    Vector b(-1,-2, -4);
+
+    REQUIRE(a+b == Vector(0, 0, -1));
+    REQUIRE_FALSE(a+b == Vector(0, 0, 1));
+
+    REQUIRE(a-b == Vector(2, 4, 7));
+
+    REQUIRE(a/5 == Vector(0.2, 0.4, 0.6));
+
+    REQUIRE(a*5 == Vector(5, 10, 15));
+}
